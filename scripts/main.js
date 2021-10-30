4a4fa36defa8369619e4e5400c195c2ebf34dd64
Martin Villanueva <martingd77@gmail.com>
2021-10-30 14:38:34 -0300

    eventos

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,14 @@
 let servicioReserva = "";
 let cantidadServicio = 0;
+let profesional = "";
 let precio = 0;
-let dia = "";
+let dia;
 
 class Reserva{
-    constructor(servicio, precio, dia, cantidad){
+    constructor(servicio, precio, profesional, dia, cantidad){
         this.servicio = servicio,
         this.precio = precio,
+        this.profesional = profesional,
         this.dia = dia,
         this.cantidad = cantidad,
         this.subTotal = 0,
@@ -22,57 +24,77 @@ class Reserva{
     }
 }
 
-
-function reservarServicio() {
-    while(!servicioReserva || servicioReserva == 0 || servicioReserva > 4) {
-        servicioReserva = parseInt(prompt("¿Qué servicio desea reservar?:\n 1: Lifting de pestañas($1500)\n 2:Lash botox($1900)\n 3:Perfilado de cejas($600)\n 4:Uñas gelificadas($2200)"));
-    }
-
-    switch(servicioReserva){
-        case 1:
-            servicioReserva = "Lifting de pestañas";
-            precio = 1500;
-            break;
-        case 2:
-            servicioReserva = "Lash botox";
-            precio = 1900;
-            break;
-        case 3:
-            servicioReserva = "Perfilado de cejas";
-            precio = 600;
-            break;
-        case 4:
-            servicioReserva = "Uñas gelificadas";
-            precio = 2200;
-            break;
-    }
-
-    while(!dia || dia == ""){
-        dia = prompt("¿Qué día desear realizarte el servicio?");
-    }
-
-    while(!cantidadServicio || cantidadServicio == 0){
-        cantidadServicio = parseInt(prompt("Servicio elegido: "+ servicioReserva + " para el día " + dia + "\n Introduzca la cantidad deseada.(sólo números)"));
-    }
-
-    return new Reserva(servicioReserva, precio, dia, cantidadServicio)
-}
+document.getElementById("agendaInput").addEventListener("blur", function() {
+    var input = this.value;
+    dia = new Date(input);
+    console.log(input);
+    console.log(dia);
+}); 
 
 
-alert("Bienvenida/o a nuestra agenda online");
+crearReserva = function(dia){
+    debugger;
+    const reserva = reservarServicio(dia);
+    reserva.calcularSubTotal();
+    reserva.calcularTotal();
 
-const reserva = reservarServicio();
+    let contenedorReserva = document.createElement("div");
 
-reserva.calcularSubTotal();
-reserva.calcularTotal();
-
-let contenedorReserva = document.createElement("div");
-
-contenedorReserva.innerHTML = `<div class="container">
+    contenedorReserva.innerHTML = `<div class="container">
                                  <h3> Detalle de la reserva: \n </h3>
                                     <p> -${reserva.servicio} x ${reserva.cantidad} : $ ${reserva.subTotal} </p>
+                                    <p> -Profesional: ${reserva.profesional} </p>
                                     <p> -Día de la reserva: ${reserva.dia} </p>
                                     <p> -Total = $ ${reserva.total}</p>
                                 </div>`;
 
-document.body.appendChild(contenedorReserva);
+    document.body.appendChild(contenedorReserva);
+}
+
+let miForm = document.getElementById("formReserva");
+miForm.addEventListener("submit", crearReserva(dia));
+
+function reservarServicio(){
+    let servicio = document.getElementById("servicioInput");
+    servicioReserva = servicio.value;
+    switch(servicioReserva){
+        case "Lifting de pestañas":
+            precio = 1500;
+            break;
+        case "Lash botox":
+            precio = 1900;
+            break;
+        case "Perfilado de cejas":
+            precio = 600;
+            break;
+        case "Uñas - Semi-permanente":
+            precio = 1500;
+            break;
+        case "Uñas - Kapping gel":
+            precio = 1800;
+            break;    
+        case "Uñas - Gelificadas":
+            precio = 2000;
+            break;
+        case "Uñas - Acrilicas":
+            precio = 2200;
+            break;
+    }
+    cantidadServicio = 1;
+       
+    /* let fechaSeleccionada = document.querySelector('input[type="date"]');
+    dia = fechaSeleccionada.value;
+    console.log(dia); */
+
+    let profesionalSeleccionada = document.getElementById("profesionalInput");
+    profesional = profesionalSeleccionada.value;
+
+    return new Reserva(servicioReserva, precio, profesional, dia, cantidadServicio);
+    
+}
+
+
+
+
+
+
