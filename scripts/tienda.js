c0a474d2574101a7c9dd11422a0694242ead3bf0
Martin Villanueva <mvillanueva@altimetrik.com>
2021-12-04 11:48:45 -0300

    carrito terminado

--- scripts/tienda.js
+++ scripts/tienda.js
@@ -12,17 +12,19 @@ $(document).ready(() => {
             let productos = respuesta;
             for (const prod of productos) {
                 $("#contProductos").append(
-                    `<div style="padding:1.2em;" id="div${prod.nombre}">
+                    `<div style="padding:1em; " id="div${prod.nombre}">
                         <image class="productos__img" src = ${prod.imagen}> </image>
                         <p>${prod.nombre} - $${prod.precio} </p>
-                        <button class="productButton btn btn-warning btn-sm" id= "btn${prod.id}">Agregar a la bolsa</button>
+                        <div class="prodButtons">
+                            <button class="productButton btn btn-warning " id= "btnadd${prod.id}">Agregar</button> 
+                            <button class="productButton btn btn-warning " id= "btnquitar${prod.id}">Quitar</button>
+                        </div>
                     </div>`
                 );
         
-                $(`#btn${prod.id}`).on('click',function () {
+                $(`#btnadd${prod.id}`).on('click',function () {
                
                 
-
                 if(productosAgregados.find( x => x.id === prod.id))
                 {
                     objIndex = productosAgregados.findIndex(x => x.id === prod.id);
@@ -64,18 +66,69 @@ $(document).ready(() => {
                             <h4 style="color:#b771f8; text-align: right;"> Total a pagar: $ ${calcularTotal(productosAgregados)}</h4>
                         </div>
                         `
-                       
-                    );                   
+                        
+                    );     
+                    localStorage.setItem("Compra Finalizada - " , compraActual);
+                });
+
+                $("#btnCerrarModal").on('click', function(){
+                    debugger;
+
+                    let modalContent = $("#modalBody");
+                    let content = modalContent.text();
+
+                     if(content.includes("Muchas gracias por su compra!")){
+                        
+                        $("#cantidadProductos").empty();
+                        $("#cantidadProductos").css("background-color","");
+                        productosAgregados = [];
+                        $("#modalBody").empty();
+                        cantProd = 0;
+                    }                
                 });
 
                 console.log(cantactual);
-        
-                $("#cantidadProductos").empty();
-                $("#cantidadProductos").css("background-color","red");
-                $("#cantidadProductos").append(cantProd);
-        
+                
+                actualizarContadorBolsa(cantProd);
+                
                 })
+
+                $(`#btnquitar${prod.id}`).on('click',function () {
+                    debugger;
+                    if(productosAgregados.find( x => x.id === prod.id)){
+                    objIndex = productosAgregados.findIndex(x => x.id === prod.id);
+                    console.log("Antes del update: ", productosAgregados[objIndex]);
+
+                    if(productosAgregados[objIndex].cantidad > 1){
+                        productosAgregados[objIndex].cantidad = productosAgregados[objIndex].cantidad - 1;
+                        console.log("Despues del update: ",productosAgregados[objIndex]);
+                        cantProd --;
+                        actualizarContadorBolsa(cantProd);
+
+                    } else if (productosAgregados[objIndex].cantidad ==1) {
+                       
+                        productosAgregados.splice(objIndex,1);
+                        cantProd --;
+                        actualizarContadorBolsa(cantProd);
+                    }
+
+                    if(productosAgregados.length == 0){
+                        limpiarContadorBolsa();
+                        cantProd = 0;
+                    }
+                   
+
+                }
+                
+                let compraActual = JSON.stringify(productosAgregados);
+
+                localStorage.setItem("Compra - " , compraActual);
+
+                })
+
             }  
+            
+
           }else{
             console.log(estado);
           }
@@ -86,14 +139,30 @@ $(document).ready(() => {
 
 function mostrarCarrito(productos){
     let carrito = "";
+    
     for(let producto of productos){
         carrito = carrito + `   <p>
                                     <img class="imgLogo" src= ${producto.imagen} />   
-                                    ${producto.descripcion} -  $${producto.precio}  &nbsp;  X ${producto.cantidad} 
-                                </p>` ;                    
-    }
+                                    ${producto.descripcion} -  $${producto.precio}  &nbsp;  X ${producto.cantidad} &nbsp; 
+                                    <button onclick="quitarProducto('${producto.id}','${productos}');" type="button"  style="border-radius: 10%; background-color: #d89ad2;" ><span aria-hidden="true">-</span></button>
+                                    <input type="image" value="" class="but-blue" onclick="quitarProducto('${producto.id}','${productos}');" />
+                                    </p>` ;                    
+                        }
+
+                        function quitarProducto(productoId, productos){
+                            debugger;
+                            let listaProductos = productos;
+                            for (let prod of listaProductos){
+                                    if(prod.id == productoId){
+                                        productos.splice(prod.id);
+                                    }
+                                }
+                                mostrarCarrito(productos);
+                            
+                            }                 
     return carrito;
 }
+
 
 
 function calcularTotal(productos){
@@ -105,4 +174,17 @@ function calcularTotal(productos){
     }
     
     return total;
+}
+
+
+function actualizarContadorBolsa(contador){
+    $("#cantidadProductos").empty();
+    $("#cantidadProductos").css("background-color","red");
+    $("#cantidadProductos").append(contador);
+}
+
+
+function limpiarContadorBolsa(){
+    $("#cantidadProductos").empty();
+    $("#cantidadProductos").css("background-color","");
 }
