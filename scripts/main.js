baa9db87ee3900e1b4b6d11409bb75e98cda27ac
Martin Villanueva <mvillanueva@altimetrik.com>
2021-12-10 16:51:47 -0300

    validaciones agregadas

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,7 @@ let profesional = "";
 let precio = 0;
 let dia;
 let hora;
+let email;
 
 let contenedorReserva = document.createElement("div");
 
@@ -15,31 +16,41 @@ document.getElementById("agendaInput").addEventListener("blur", function() {
 }); 
 
 function crearReserva(e){
+    e.preventDefault();
+    if(validarFormulario()){
+       
+        if(contenedorReserva.innerHTML != ""){
+            contenedorReserva.innerHTML = "";
+        }
+        let form = e.target;
+       
+        const reserva = reservarServicio();
+        reserva.calcularSubTotal();
+        reserva.calcularTotal();
+    
+        //jQuery
+        $("#exampleModalBody").empty();
+        $("#exampleModalBody").append(`<div class="container">
+                            <p> -${reserva.servicio} x ${reserva.cantidad} : $ ${reserva.subTotal} </p>
+                            <p> -Profesional: ${reserva.profesional} </p>
+                            <p> -Día de la reserva: ${reserva.dia} a las ${reserva.hora}</p>
+                            <p style="font-weight: bold;"> -Total = $ ${reserva.total}</p>
+                            <p> Se enviará un correo electrónico de confirmacion a : ${email}</p>
+                        </div>`
+        );
+        
+        $(function(){
+            $("#exampleModal").modal("show");
+        });
 
-    if(contenedorReserva.innerHTML != ""){
-        contenedorReserva.innerHTML = "";
+        let reservaJson = JSON.stringify(reserva);
+    
+        localStorage.setItem("Reserva - " + reserva.servicio + " - " + reserva.profesional, reservaJson);
+    }
+    else{
+        alert("Hubo un problema al guardar la reserva. Revisa bien todos los campos e intentalo nuevamente.")
     }
     
-    e.preventDefault();
-    let form = e.target;
-   
-    const reserva = reservarServicio();
-    reserva.calcularSubTotal();
-    reserva.calcularTotal();
-
-    //jQuery
-    $("body").append(`<div class="container">
-                        <h3> Detalle de la reserva: \n </h3>
-                        <p> -${reserva.servicio} x ${reserva.cantidad} : $ ${reserva.subTotal} </p>
-                        <p> -Profesional: ${reserva.profesional} </p>
-                        <p> -Día de la reserva: ${reserva.dia} a las ${reserva.hora}</p>
-                        <p> -Total = $ ${reserva.total}</p>
-                    </div>`
-    );
-
-    let reservaJson = JSON.stringify(reserva);
-
-    localStorage.setItem("Reserva - " + reserva.servicio + " - " + reserva.profesional, reservaJson);
 }
 
 let miForm = document.getElementById("formReserva"); 
@@ -75,6 +86,8 @@ function reservarServicio(){
     profesional = $("#profesionalInput").val();
 
     hora = $("#horarioInput").val();
+
+    email = $("#emailInput").val();
 
     return new Reserva(servicioReserva, precio, profesional, dia, hora, cantidadServicio);
 }
@@ -123,7 +136,9 @@ $(function () {
 
     var profesionalServicio = {
         Alejandra: ["Lifting de pestañas", "Lash botox", "Perfilado de cejas"],
-        Mayra: ["Uñas - Semi-permanente", "Uñas - Kapping gel", "Uñas - Gelificadas", "Uñas - Acrilicas"]
+        Mayra: ["Uñas - Semi-permanente", "Uñas - Kapping gel", "Uñas - Gelificadas", "Uñas - Acrilicas"],
+        "Selecciona una profesional": ["Selecciona un servicio"] 
+
     }
     
     $prof.change(function () {
@@ -133,4 +148,105 @@ $(function () {
         }));
     });
 
+});
+
+function validarFormulario(){
+    
+    let profesionalSeleccionado = document.forms["formReserva"]["profesionalInput"].value;
+    let servicioSeleccionado = document.forms["formReserva"]["servicioInput"].value;
+    let email = document.forms["formReserva"]["emailInput"].value;
+   
+    if (profesionalSeleccionado == "Selecciona una profesional") {
+        $("#errorProfesionalInput").append(`<p> Para reservar es necesario seleccionar una profesional  </p>`);
+        return false;
+    }
+
+    if (servicioSeleccionado == "Selecciona un servicio" || servicioSeleccionado == "") {
+        $("#errorServicioInput").append(`<p> Para reservar es necesario seleccionar una servicio  </p>`);
+        return false;
+    }
+    
+    if(dia == "" || dia == undefined){
+        $("#errorAgendaInput").append(`<p> Para reservar es necesario elegir una fecha  </p>`);
+        return false;
+    }
+
+    if (!validateEmail(email)) {
+        $("#errorEmailInput").append(`<p> El correo electrónico ingresado no es válido  </p>`);
+        return false;
+    }
+    
+
+    $("#errorProfesionalInput").empty();
+    $("#errorServicioInput").empty();
+    $("#errorAgendaInput").empty();
+    
+    return true;
+
+}
+
+$("#btnCerrar").on('click', function(){
+    $("#exampleModalBody").empty();
+    $("#exampleModal").modal("hide");
+    location.reload();
+
+});
+
+
+const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+  $("#emailInput").focusout(function(){
+      if(!validateEmail(this.value)){
+        $("#emailInput").css('border-color', 'red');
+      }
+      else
+      {
+        $("#emailInput").css('border-color', 'green');
+      }
+  });
+
+
+  $("#profesionalInput").focusout(function(){
+    if((this.value == "Selecciona una profesional")){
+      $("#profesionalInput").css('border-color', 'red');
+    }
+    else
+    {
+      $("#profesionalInput").css('border-color', 'green');
+    }
+});
+
+$("#servicioInput").focusout(function(){
+    if((this.value == "Selecciona un servicio")){
+      $("#servicioInput").css('border-color', 'red');
+    }
+    else
+    {
+      $("#servicioInput").css('border-color', 'green');
+    }
+});
+
+$("#agendaInput").focusout(function(){
+
+    let fechaElegida = this.value;
+    let fechaHoy = new Date();
+
+    if((fechaElegida == "" || fechaElegida == undefined)){
+      $("#agendaInput").css('border-color', 'red');
+    }
+    else if (new Date(fechaElegida) <= fechaHoy){
+        $("#errorAgendaInput").append(`<p> Para reservar es necesario elegir una fecha posterior a hoy  </p>`);
+        $("#agendaInput").css('border-color', 'red');
+    }
+    else
+    {
+      $("#errorAgendaInput").empty();
+      $("#agendaInput").css('border-color', 'green');
+    }
 });
